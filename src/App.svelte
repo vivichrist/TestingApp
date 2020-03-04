cbeeda2fefc1ba38450b2f41326a1a448dcfd224
Vivian Stewart <vivichrist@gmail.com>
2020-03-05 11:03:38 +1300

    move functions to top level

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,5 @@
 <script>
-	let identity = {signedIn: false};
-
-	export const onSignIn = (googleUser) => {
-        // Useful data for your client-side scripts:
-        var profile = googleUser.getBasicProfile();
-        // The ID token you need to pass to your backend:
-        identity = {
-        	signedIn: true,
-            id: profile.getId(),
-            name: profile.getName(),
-            given_name: profile.getGivenName(),
-            family_name: profile.getFamilyName(),
-            img_url: profile.getImageUrl(),
-            email: profile.getEmail(),
-            token: googleUser.getAuthResponse().id_token,
-        };
-    }
-
-    export const signOut = () => {
-		var auth2 = gapi.auth2.getAuthInstance();
-		auth2.signOut().then(function () {
-			console.log('User signed out.');
-		});
-		identity.setState( {signedIn: false} );
-	}
+	export let identity = {signedIn: false};
 </script>
 
 <main>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
 import App from './App.svelte';
 
+const onSignIn = (googleUser) => {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    // The ID token you need to pass to your backend:
+    identity = {
+        signedIn: true,
+        id: profile.getId(),
+        name: profile.getName(),
+        given_name: profile.getGivenName(),
+        family_name: profile.getFamilyName(),
+        img_url: profile.getImageUrl(),
+        email: profile.getEmail(),
+        token: googleUser.getAuthResponse().id_token,
+    };
+}
+
+const signOut = () => {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    identity.setState( {signedIn: false} );
+}
+
 const app = new App({
 	target: document.body,
 	props: {
-		onSignIn,
-		signOut,
+		identity,
 	}
 });
 
