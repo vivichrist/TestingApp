c46a83c3d3eccfa95dd228a4ea101a58ab1f5635
Vivian Stewart <vivichrist@gmail.com>
2020-03-05 13:27:11 +1300

    no setstate

--- src/main.js
+++ src/main.js
@@ -4,24 +4,23 @@ const onSignIn = (googleUser) => {
     // Useful data for your client-side scripts:
     var profile = googleUser.getBasicProfile();
     // The ID token you need to pass to your backend:
-    identity = {
-        signedIn: true,
-        id: profile.getId(),
-        name: profile.getName(),
-        given_name: profile.getGivenName(),
-        family_name: profile.getFamilyName(),
-        img_url: profile.getImageUrl(),
-        email: profile.getEmail(),
-        token: googleUser.getAuthResponse().id_token,
-    };
+    identity.id = profile.getId();
+    identity.name = profile.getName();
+    identity.given_name = profile.getGivenName();
+    identity.family_name = profile.getFamilyName();
+    identity.img_url = profile.getImageUrl();
+    identity.email = profile.getEmail();
+    identity.token = googleUser.getAuthResponse().id_token;
+
+    identity.signedIn = true;
 }
 
 const signOut = () => {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
+        identity.signedIn = false;
     });
-    identity.setState( {signedIn: false} );
 }
 
 const app = new App({
