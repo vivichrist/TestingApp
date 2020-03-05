89b962278f8c531894e36e9ba6d3c211d4bfdf5a
Vivian Stewart <vivichrist@gmail.com>
2020-03-05 13:47:59 +1300

    try main button

--- src/main.js
+++ src/main.js
@@ -18,7 +18,6 @@ const onSignIn = (googleUser) => {
 const signOut = () => {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        console.log('User signed out.');
         identity.signedIn = false;
     });
 }
