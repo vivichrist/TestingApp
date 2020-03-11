a26d86e5da62a0fd72aba852d0b3d16d1d43f937
Vivian Stewart <vivichrist@gmail.com>
2020-03-11 16:30:50 +1300

    Custom Button

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Login from './Login.svelte'
+
   export let signedIn = false;
 
   let ident = {
@@ -11,9 +13,10 @@
     token: "",
   };
 
-  window.onSignIn = (googleUser) => {
+
+  window.onSignIn = (user) => {
       // Useful data for your client-side scripts:
-      const profile = googleUser.getBasicProfile();
+      const profile = user.getBasicProfile();
       // The ID token you need to pass to your backend:
       ident.id = profile.getId();
       ident.name = profile.getName();
@@ -21,12 +24,31 @@
       ident.family_name = profile.getFamilyName();
       ident.img_url = profile.getImageUrl();
       ident.email = profile.getEmail();
-      ident.token = googleUser.getAuthResponse().id_token;
+      ident.token = user.getAuthResponse().id_token;
 
       signedIn = true;
-      document.getElementById("signinButton").style.display = "none";
+      document.getElementById("SignInWrapper").style.display = "none";
       console.log("onsign-in");
   }
+
+  window.startApp = function() {
+    gapi.load('auth2', function(){
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      let auth2 = gapi.auth2.init({
+        client_id: '461979086851-bbjhehqb9qpn3boroii8l4klmcc718bd.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email',
+      });
+      let element = document.getElementById('customBtn');
+      auth2.attachClickHandler(element, {},
+        function(user) {
+          onSignIn(user);
+        }, function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        }
+      );
+    });
+  };
 
   window.signOut = () => {
     const auth2 = gapi.auth2.getAuthInstance();
@@ -40,14 +62,14 @@
   		ident.token = "";
 
       signedIn = false;
-      document.getElementById("signinButton").style.display = "block";
+      document.getElementById("SignInWrapper").style.display = "block";
       console.log("onsign-out");
     });
   }
 </script>
 
 <svelte:head>
-	<script src="https://apis.google.com/js/platform.js" async defer></script>
+	<script src="https://apis.google.com/js/api:client.js"></script>
 </svelte:head>
 
 <main>
@@ -56,6 +78,8 @@
 		<img src={ident.img_url} />
 		<p>{ident.email}</p>
 		<button onclick="signOut()">Sign Out</button>
+  {:else}
+    <Login />
 	{/if}
 </main>
 
