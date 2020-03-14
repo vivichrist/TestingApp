35fc2feccd1f96849946dbea28e90f1a738181f0
Vivian Stewart <vivichrist@gmail.com>
2020-03-14 13:38:56 +1300

    First page

---
+++ src/Page1.svelte
@@ -0,0 +1,36 @@
+
+<style>
+  header {
+    padding: 2em;
+  }
+  li {
+    margin-right: 2rem;
+  }
+</style>
+
+<header class="bg-info">
+  <ul class="nav nav-tabs">
+    <li class="nav-Item">
+      <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+        Icealicous
+      </button>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Link 1</a>
+        <a class="dropdown-item" href="#">Link 2</a>
+        <a class="dropdown-item" href="#">Link 3</a>
+      </div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#">Disabled</a>
+    </li>
+  </ul>
+</header>
