b93dfb6dc247d9b6de4ad4b27d4036629955685b
Vivian Stewart <vivichrist@gmail.com>
2020-04-05 11:58:55 +1200

    remove the border from around the sorting icon

--- src/RuleColumn.svelte
+++ src/RuleColumn.svelte
@@ -1,36 +1,15 @@
 <script>
-    let sorts = [
-        "/agiledata-logo-icon-purple-dropdown.svg",
-        "/agiledata-logo-icon-purple-dropdown.svg",
-        "/agiledata-logo-icon-grey-dropdown.svg"
-    ];
-    export let name = "";
-    export let filter = "";
-    export let column = "";
-    export let callback = () => {};
-    export let sort = 2;
+  let sorts = [
+    "/agiledata-logo-icon-purple-dropdown.svg",
+    "/agiledata-logo-icon-purple-dropdown.svg",
+    "/agiledata-logo-icon-grey-dropdown.svg"
+  ];
+  export let name = "";
+  export let filter = "";
+  export let column = "";
+  export let callback = () => {};
+  export let sort = 2;
 </script>
-
-<th scope="col">
-    <div class="input-group">
-        <input type="text" class="form-control"
-               placeholder={name} bind:value={filter} />
-        <div class="input-group-append" on:click={callback}>
-            <span class="input-group-text bg-white border-light m-0 p-0">
-            {#if column == name}
-                {#if sort == 1}
-                    <img width=15 height=15 src={sorts[sort]}
-                         style="transform: rotate(180deg);" alt="sorting" />
-                {:else}
-                    <img width=15 height=15 src={sorts[sort]} alt="sorting" />
-                {/if}
-            {:else}
-                <img width=15 height=15 src={sorts[2]} alt="sorting" />
-            {/if}
-            </span>
-        </div>
-    </div>
-</th>
 
 <style>
   input::placeholder {
@@ -49,7 +28,6 @@
     }
   }
   @media screen and (max-width: 1260px) {
-
     input {
       max-width: 12vw;
       font-size: 10pt;
@@ -62,3 +40,31 @@
     }
   }
 </style>
+
+<th scope="col">
+  <div class="input-group">
+    <input
+      type="text"
+      class="form-control"
+      placeholder={name}
+      bind:value={filter} />
+    <div class="input-group-append" on:click={callback}>
+      <span class="input-group-text bg-white border-0 m-0 p-0">
+        {#if column == name}
+          {#if sort == 1}
+            <img
+              width="15"
+              height="15"
+              src={sorts[sort]}
+              style="transform: rotate(180deg);"
+              alt="sorting" />
+          {:else}
+            <img width="15" height="15" src={sorts[sort]} alt="sorting" />
+          {/if}
+        {:else}
+          <img width="15" height="15" src={sorts[2]} alt="sorting" />
+        {/if}
+      </span>
+    </div>
+  </div>
+</th>
