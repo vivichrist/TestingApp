4376bf2a2f7da596fb7cb712d9df3b2bdf50a6d8
Vivian Stewart <vivichrist@gmail.com>
2020-04-05 11:53:06 +1200

    new login

--- src/Login.svelte
+++ src/Login.svelte
@@ -2,8 +2,8 @@
   import Email from './Email.svelte';
 
   let images = [
-    "/img/beach-island.png",
-    "/img/beach-sunset.png"
+    "/img/beach-island.webp",
+    "/img/beach-sunset.webp"
   ];
 
   const rndImage = () => {
@@ -11,79 +11,60 @@
   };
 </script>
 
-<div class="container-fluid bgc" style="background-image: url({rndImage()});">
-  <div class="mx-auto">
-    <img src="agiledata-logo-full-purple.svg" alt="agile data logo" id="logo"/>
-  </div>
-  <div id="SignInWrapper" class="mx-auto">
-    <Email icon="at" hint="email"/>
-    <div id="customBtn" class="SignIn btn btn-outline-primary bg-primary"
-                        on:click="{onSignIn}">
-      <span class="text-info">Login</span>
+<div class="d-flex flex-column vw-100 align-items-center justify-content-between bgc" style="background-image: url({rndImage()});">
+  <div id="SignInWrapper" class="d-block align-center flex-grow-1">
+    <!-- <Email icon="at" hint="email"/> -->
+    <div class="d-block lc">
+      <img src="agiledata-logo-icon-white.svg" alt="agile data logo" id="logo"/>
+    </div>
+    <div id="customBtn" class="SignIn btn btn-outline-primary"
+                        on:click="{onSignIn}" role="button">
+      <span class="text-info align-center">Login</span>
     </div>
   </div>
+  <footer>
+    <span type="button" class="text-white mr-5">Terms of Use</span>
+    <span type="button" class="text-white ml-5">Privacy</span>
+  </footer>
 </div>
 
 <style>
+  #logo {
+    height: 80px;
+    box-shadow: none;
+  }
+  .lc {
+    margin-top: 35vh;
+  }
+  footer {
+    border-top: 3px solid white;
+    padding: .5rem 1rem 0 1rem;
+    margin-bottom: 2rem;
+  }
+  footer>span {
+    font-size: 16pt;
+    border-width: 2px;
+  }
   .bgc {
     height: 100vh;
     background-position: center;
     background-size: 100vw;
   }
   #customBtn {
+    background-color: #755A9955;
+    background-blend-mode: color-burn;
     margin-top: 2vh;
+    width: 15rem;
   }
   #customBtn:hover {
     cursor: pointer;
   }
-  @media only screen and (orientation: landscape) {
+  /* @media only screen and (orientation: landscape) {
     @media screen and (min-width: 740px) {
-      #SignInWrapper {
-        margin-top: 25vh;
-        margin-bottom: 10vh;
-        padding-left: 15vw;
-        padding-right: 15vw;
-        text-align: right;
-        width: 90vw;
-      }
-      #logo {
-        padding-top: 25vh;
-        padding-left: 10%;
-        box-shadow: none;
-        width: calc(30vw + 30vh);
-      }
     }
     @media screen and (max-width: 740px) {
-      #SignInWrapper {
-        margin-top: 25vh;
-        margin-bottom: 0;
-        padding-left: 0;
-        padding-right: 0;
-        text-align: right;
-        width: 80vw;
-      }
-      #logo {
-        padding-top: 20vh;
-        padding-left: 5%;
-        box-shadow: none;
-        width: calc(30vw + 30vh);
-      }
     }
   }
   @media only screen and (orientation: portrait) {
-    #SignInWrapper {
-      margin-top: 40vh;
-      margin-bottom: 30vh;
-      padding-left: 2vw;
-      padding-right: 2vw;
-      text-align: right;
-      width: 95vw;
-    }
-    #logo {
-      padding-top: 2vh;
-      padding-left: 2%;
-      box-shadow: none;
-      width: 95vw;
-    }
-  }
+  } */
 </style>
