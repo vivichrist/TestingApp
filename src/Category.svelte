a20cb89a37e40487b27e487031af1dcc7e88fb5d
Vivian Stewart <vivichrist@gmail.com>
2020-04-17 23:27:54 +1200

    Right corrner icon-buttons

--- src/Category.svelte
+++ src/Category.svelte
@@ -66,6 +66,19 @@
                  data-trigger="focus"
                  data-content="{item.topics}"
                  on:contextmenu|preventDefault={handlePopup}>
+              <span class="float-right m-0 p-0">
+                <button class="btn btn-primary btn-sm p-0 m-0 card-icon">
+                  <svg class="bi bi-pencil m-0 p-0" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
+                    <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
+                  </svg>
+                </button>
+                <button class="btn btn-primary m-0 p-0 btn-sm card-icon">
+                  <svg class="bi bi-three-dots-vertical" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm0-5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm0-5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" clip-rule="evenodd"/>
+                  </svg>
+                </button>
+              </span>
               <h6 class="card-title text-secondary flex-wrap">{item.alias}</h6>
               <p class="card-text my-1">
               {#each item.topics as token}
@@ -90,6 +103,19 @@
                  data-trigger="focus"
                  data-content="{item.topics}"
                  on:contextmenu|preventDefault={handlePopup}>
+              <span class="float-right m-0 p-0">
+                <button class="btn btn-primary m-0 p-0 btn-sm card-icon">
+                  <svg class="bi bi-pencil m-0 p-0" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
+                    <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
+                  </svg>
+                </button>
+                <button class="btn btn-primary m-0 p-0 btn-sm card-icon">
+                  <svg class="bi bi-three-dots-vertical" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm0-5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm0-5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" clip-rule="evenodd"/>
+                  </svg>
+                </button>
+              </span>
               <h6 class="card-title text-secondary flex-wrap">{item.alias}</h6>
               <p class="card-text my-1">
               {#each item.topics as token}
@@ -120,6 +146,9 @@
 </div>
 
 <style>
+  .card-icon {
+    padding-left: 0.33em;
+  }
   h6 {
     font-size: 15pt;
   }
